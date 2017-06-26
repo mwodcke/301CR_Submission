a38c704bdfea8f2df5cfa5ac007e8ba04b636667
Mitch Wodcke <mwodcke@hotmail.co.uk>
2017-06-26 20:55:14 +0100

    Initial Stage of Game

---
+++ Submission_Game/Submission_Game/Player.cpp
@@ -0,0 +1,27 @@
+#include "Player.h"
+
+
+Player::Player()
+{
+}
+
+
+Player::~Player()
+{
+}
+
+// Player Keyboard Input
+void keyInput(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+		// Moves Upwards
+	case'w':
+		break;
+		// Moves Downwards
+	case's':
+		break;
+	default:
+		break;
+	}
+}
